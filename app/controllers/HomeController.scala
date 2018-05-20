d46b4ed9aedef9754c399d4920c2d5f820623f20
rubenamcruz <rubenamcruz@gmail.com>
2018-05-20 17:31:58 +0100

    changing and deleting files

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -17,7 +17,7 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * a path of `/`.
    */
   def index = Action {
-    Ok(views.html.index2()) 
+    Ok(views.html.index())
   }
 
   def about = Action {
