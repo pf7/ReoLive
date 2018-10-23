873591e66cdbee3680d2bb517d80534674b2a250
Jose Proenca <jose@proenca.org>
2018-10-23 23:33:02 +0100

    new snapshot - with support for containers and syntax highlighting

--- full_compile.sh
+++ full_compile.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
+# Compile all JavaScript (JS), both in localJS and in RemoteJS
 sbt fullOptJS
-rm server/app/assets/javascripts/local_js-opt.js
-cp localJS/target/scala-2.12/local_js-opt.js server/app/assets/javascripts/local_js-opt.js
-rm server/app/assets/javascripts/remote_js-opt.js
-cp remoteJS/target/scala-2.12/remote_js-opt.js server/app/assets/javascripts/remote_js-opt.js
+
+# Copy JS from localJS to the server
+cp localJS/target/scala-2.12/local_js-opt.js \
+   server/app/assets/javascripts/local_js-opt.js
+cp localJS/target/scala-2.12/local_js-opt.js.map \
+   server/app/assets/javascripts/
+
+# Copy JS from remoteJS to the server
+cp remoteJS/target/scala-2.12/remote_js-opt.js \
+   server/app/assets/javascripts/remote_js-opt.js
+cp remoteJS/target/scala-2.12/remote_js-opt.js.map \
+   server/app/assets/javascripts/
+
+# Copy stylesheet from localJS to server
+cp localJS/src/main/resources/css/style.css \
+   server/app/assets/stylesheets/
+
+# Compile the server, after having the updated JS
 sbt server/compile
+
+echo "\n - Run the server using 'sbt server/run', and access it via http://localhost:9000."
