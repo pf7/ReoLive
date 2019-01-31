55d1043a35bb2f33c5c4457bd8ac61aa71a91bbf
Guillermina Cledou <guillecledou@gmail.com>
2019-01-31 23:33:00 +0000

    Virtuoso automata box

---
+++ commonJS/src/main/scala/common/widgets/virtuoso/VirtuosoAutomataBox.scala
@@ -0,0 +1,57 @@
+package common.widgets.virtuoso
+
+import common.frontend.AutomataToJS
+import common.widgets.{Box, GraphBox, OutputArea}
+import hub.HubAutomata
+import org.scalajs.dom
+import org.scalajs.dom.{MouseEvent, html}
+import preo.ast.CoreConnector
+import preo.backend.{Automata, PortAutomata}
+
+/**
+  * Created by guille on 31/01/2019
+  */
+
+
+class VirtuosoAutomataBox(dependency: Box[CoreConnector], errorBox: OutputArea)
+  extends Box[Automata]("Automaton of the instance", List(dependency)) {
+  private var svg: Block = _
+  private var automaton: Automata = _
+
+
+  private val widthAutRatio = 7
+  private val heightAutRatio = 3
+  private val densityAut = 0.2 // nodes per 100x100 px
+
+  override def get: Automata = automaton
+
+  override def init(div: Block, visible: Boolean): Unit = {
+    svg= GraphBox.appendSvg(panelBox(div, visible),"automata")
+    dom.document.getElementById("Automaton of the instance").firstChild.firstChild.firstChild.asInstanceOf[html.Element]
+      .onclick = {e: MouseEvent => if(!isVisible) drawAutomata() else deleteAutomaton()}
+
+  }
+
+  override def update(): Unit = if(isVisible) drawAutomata()
+
+
+  private def drawAutomata(): Unit =
+    try{
+      automaton = Automata[HubAutomata](dependency.get)
+      val sizeAut = automaton.getStates.size
+      //              println("########")
+      //              println(aut)
+      //              println("++++++++")
+      val factorAut = Math.sqrt(sizeAut * 10000 / (densityAut * widthAutRatio * heightAutRatio))
+      val width = (widthAutRatio * factorAut).toInt
+      val height = (heightAutRatio * factorAut).toInt
+      svg.attr("viewBox", s"00 00 $width $height")
+
+      scalajs.js.eval(AutomataToJS(automaton))
+    }
+    catch Box.checkExceptions(errorBox)
+
+  private def deleteAutomaton(): Unit = {
+    svg.selectAll("g").html("")
+  }
+}
---
+++ commonJS/src/main/scala/common/widgets/virtuoso/VirtuosoGraphBox.scala
@@ -0,0 +1,32 @@
+package common.widgets.virtuoso
+
+import common.frontend.GraphsToJS
+import common.widgets.{Box, GraphBox, OutputArea}
+import preo.ast.CoreConnector
+import preo.backend.Graph
+
+
+/**
+  * Created by guillecledou on 31/01/2019
+  */
+
+
+class VirtuosoGraphBox(dependency: Box[CoreConnector], errorBox: OutputArea)
+  extends GraphBox(dependency,errorBox) {
+
+//  override  def toJs(g:Graph):Unit = scalajs.js.eval(GraphsToJS(g))
+
+  override protected def drawGraph(): Unit = try{
+    graph = Graph.connToNodeGraph(dependency.get,true)
+    val size = graph.nodes.size
+    val factor = Math.sqrt(size * 10000 / (densityCirc * widthCircRatio * heightCircRatio))
+    val width = (widthCircRatio * factor).toInt
+    val height = (heightCircRatio * factor).toInt
+    box.attr("viewBox", s"00 00 $width $height")
+    println(dependency.get)
+    println(graph)
+//        toJs(graph)
+        scalajs.js.eval(GraphsToJS.toVirtuosoJs(graph))
+  }
+  catch Box.checkExceptions(errorBox)
+}
